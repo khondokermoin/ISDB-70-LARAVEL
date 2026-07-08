8f03af91423632715bb3c1f0fb9da08a09950e3b
khondokermoin <mouin2k19@gmail.com>
2026-07-08 14:27:50 +0600

    assets files add

--- laravel/project/Advanced_Cloud_POS_Inventory_SaaS/cloud-pos-inventory/resources/views/auth/login.blade.php
+++ laravel/project/Advanced_Cloud_POS_Inventory_SaaS/cloud-pos-inventory/resources/views/auth/login.blade.php
@@ -9,38 +9,38 @@
     <meta content="Coderthemes" name="author" />
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('') }}build/frontend_assets/images/favicon.ico">
+    <link rel="shortcut icon" href="{{ asset('') }}frontend_assets/images/favicon.ico">
 
     <!-- Theme Config Js -->
-    <script src="{{ asset('') }}build/frontend_assets/js/config.js"></script>
+    <script src="{{ asset('') }}frontend_assets/js/config.js"></script>
 
     <!-- Vendor css -->
-    <link href="{{ asset('') }}build/frontend_assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('') }}frontend_assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
 
     <!-- App css -->
-    <link href="{{ asset('') }}build/frontend_assets/css/app.min.css" rel="stylesheet" type="text/css"
+    <link href="{{ asset('') }}frontend_assets/css/app.min.css" rel="stylesheet" type="text/css"
         id="app-style" />
 
     <!-- Icons css -->
-    <link href="{{ asset('') }}build/frontend_assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('') }}frontend_assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 
     <div class="auth-bg d-flex min-vh-100 justify-content-center align-items-start">
-        <div class="row g-0 justify-content-center w-100 m-xxl-5 px-xxl-4 m-3">
+        <div class="m-3 row g-0 justify-content-center w-100 m-xxl-5 px-xxl-4">
             <div class="col-xl-4 col-lg-5 col-md-6">
-                <div class="card overflow-hidden text-center p-xxl-4 p-3 mb-0">
-                    <a href="index.html" class="auth-brand mb-4">
-                        <img src="{{ asset('') }}build/frontend_assets/images/logo-dark.png" alt="dark logo"
+                <div class="p-3 mb-0 overflow-hidden text-center card p-xxl-4">
+                    <a href="index.html" class="mb-4 auth-brand">
+                        <img src="{{ asset('') }}frontend_assets/images/logo-dark.png" alt="dark logo"
                             height="26" class="logo-dark">
-                        <img src="{{ asset('') }}build/frontend_assets/images/logo.png" alt="logo light"
+                        <img src="{{ asset('') }}frontend_assets/images/logo.png" alt="logo light"
                             height="26" class="logo-light">
                     </a>
 
-                    <h4 class="fw-semibold mb-2 fs-18">Log In to your account</h4>
+                    <h4 class="mb-2 fw-semibold fs-18">Log In to your account</h4>
 
-                    <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
+                    <p class="mb-4 text-muted">Enter your email address and password to access admin panel.</p>
 
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -74,13 +74,13 @@
                             @enderror
                         </div>
 
-                        <div class="d-flex justify-content-between mb-3">
+                        <div class="mb-3 d-flex justify-content-between">
                             <div class="form-check">
                                 <input type="checkbox" id="remember" name="remember" class="form-check-input">
                                 <label class="form-check-label" for="checkbox-signin">Remember me</label>
                             </div>
 
-                            <a href="auth-recoverpw.html" class="text-muted border-bottom border-dashed">Forget
+                            <a href="auth-recoverpw.html" class="border-dashed text-muted border-bottom">Forget
                                 Password</a>
                         </div>
 
@@ -91,8 +91,8 @@
 
                 </div>
 
-                <div class="text-center mt-3">
-                    <p class="fs-14 mb-4">Don't have an account? <a href="auth-register.html"
+                <div class="mt-3 text-center">
+                    <p class="mb-4 fs-14">Don't have an account? <a href="auth-register.html"
                             class="fw-semibold text-danger ms-1">Sign Up !</a></p>
 
                     <p class="mt-auto mb-0">
@@ -107,10 +107,10 @@
     </div>
 
     <!-- Vendor js -->
-    <script src="{{ asset('') }}build/frontend_assets/js/vendor.min.js"></script>
+    <script src="{{ asset('') }}frontend_assets/js/vendor.min.js"></script>
 
     <!-- App js -->
-    <script src="{{ asset('') }}build/frontend_assets/js/app.js"></script>
+    <script src="{{ asset('') }}frontend_assets/js/app.js"></script>
 
 </body>
 
